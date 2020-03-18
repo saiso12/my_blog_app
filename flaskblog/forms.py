09f71114fd52d50e6f169ba16f4acd7643cf1aa1
gighead <saiprash@outlook.com>
2020-03-18 15:12:55 -0700

    1. added login and the creds will be updated to DB 2. Password hashed and inserted into DB 3. Added a new route for logout 4. Added a new route for account 5. UI change when logged in and log out 6. New account html pages for account and login

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,15 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
     """Registration forms and user inputs for the blog app"""
 
     # defining class variables for instantiating fields
-    username = StringField(
-        "Username", validators=[DataRequired(), Length(min=2, max=20)]
-    )
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField("Email", validators=[DataRequired(), Email()])
 
     password = PasswordField("Password", validators=[DataRequired()])
@@ -18,6 +17,20 @@ class RegistrationForm(FlaskForm):
     )
 
     submit = SubmitField("Sign Up")
+
+    #validate username field for the forms
+    def validate_username(self, username):
+        """ check if username entered already exists in the db"""
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose another one.')
+    
+    def validate_email(self, email):
+        """check if email entered already exists in db"""
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('That email is taken. Please choose another email')
+
 
 
 class LoginForm(FlaskForm):
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,7 +1,15 @@
 from datetime import datetime
-from flaskblog import db
+from flaskblog import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    """ reloading the user from the user_id stored in the session"""
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -22,4 +30,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
-
