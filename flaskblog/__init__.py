327ab9f768dff92f06aae8aa1533a1faa956b7a3
gighead <saiprash@outlook.com>
2020-03-18 15:12:55 -0700

    1. added login and the creds will be updated to DB 2. Password hashed and inserted into DB 3. Added a new route for logout 4. Added a new route for account 5. UI change when logged in and log out 6. New account html pages for account and login

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 
@@ -7,7 +9,12 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "0e1d69604f9e7bcde06a39ef3afc18e6"
-app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/flask-db/db.sqlite'
 db = SQLAlchemy(app)
+db.create_all()
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
