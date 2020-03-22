9c6401c20d5ff4b775af81d16df6e28aec1cfe9c
gighead <saiprash@outlook.com>
2020-03-22 16:17:44 -0700

    1. New class to accomodate update accounts(email,username, and profile pic) 2. /account route for GET and POST 3. save_picture function to resize pic to 125pix and store in file system 4. rended update account info on account & register HTML's 5. added comments

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -26,7 +28,7 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('That username is taken. Please choose another one.')
     
     def validate_email(self, email):
-        """check if email entered already exists in db"""
+        """check if email entered  during user reg already exists in db """
         email = User.query.filter_by(email=email.data).first()
         if email:
             raise ValidationError('That email is taken. Please choose another email')
@@ -43,3 +45,27 @@ class LoginForm(FlaskForm):
     remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
 
+class UpdateAccountForm(FlaskForm):
+    """Updating Account Info for users"""
+
+    # defining class variables for instantiating fields
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField("Update")
+
+    #validate username field for the forms
+    def validate_username(self, username):
+        """ check if username entered already exists in the db when updating Account info"""
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another one.')
+    
+    def validate_email(self, email):
+        """check if email entered  during user reg already exists in db  updating Account info"""
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That email is taken. Please choose another email')
