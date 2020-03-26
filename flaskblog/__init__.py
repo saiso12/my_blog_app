8429ebbab54e93e58eea0c4783cb4df69bbf429a
gighead <saiprash@outlook.com>
2020-03-25 17:39:58 -0700

    1. sqllite fix for local dev 2. added new routes to perfrom update and delete posts for users 3. layout change for new posts 4. css model inclusion for deleting posts 5. refactored html

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "0e1d69604f9e7bcde06a39ef3afc18e6"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/flask-db/db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///est.db'
 db = SQLAlchemy(app)
 db.create_all()
 bcrypt = Bcrypt(app)
